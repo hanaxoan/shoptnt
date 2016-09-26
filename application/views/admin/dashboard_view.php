911530a18f104aeccbe10b397534b0648fbd1878
Nguyen Tuan <nguyentientuan@luvina.net>
2016-09-26 14:00:54 +0700

    Commit code

---
+++ application/views/admin/dashboard_view.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to the Dashboard</title>
+    </head>
+    <body>
+        <div id="container">
+            <h1>Welcome to the Dashboard</h1>
+            <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.
+        </div>
+    </body>
+</html>
---
+++ application/views/templates/_parts/admin_master_header_view.php
@@ -0,0 +1,2 @@
+<?php define('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
